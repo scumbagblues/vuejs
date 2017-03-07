188433e94b8b0a4d1cbaacaf2151d9514321ce8a
vagrant <vagrant@homestead>
2017-03-07 21:05:17 +0000

    Innitial commit

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Data;
+
+class MainController extends Controller
+{
+    public function storeItem(Request $request) {
+        $data = new Data ();
+        $data->name = $request->name;
+        $data->save ();
+        return $data;
+    }
+    public function readItems() {
+        $data = Data::all ();
+        return $data;
+    }
+    public function deleteItem(Request $request) {
+        $data = Data::find ( $request->id )->delete ();
+    }
+}
